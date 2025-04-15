e0cd07cff2e7c1ffb49044155de536bc74f9ee2c
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-04-15 19:50:28 -0300

    Configurando o banco de dados

---
+++ prisma/migrations/20250415224827_create_table_usuario/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "usuarios" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "nome" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "senha" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "usuarios_email_key" ON "usuarios"("email");
